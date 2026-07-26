8448f60c753df1f3ca51d4e8bf8908347a2182d6
Ashish15121986 <116809169+Ashish15121986@users.noreply.github.com>
2026-07-26 17:37:43 +0530

    Update greeting message in Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello I am Ashish Srivastava, I am fine");
+		System.out.println("Hello I am Ashish Srivastava, I am fine automatically run");
 	}
 }
 
