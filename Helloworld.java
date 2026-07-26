6297858c54cb433c520d6c3fe3f123d8e4c8f94e
Ashish15121986 <116809169+Ashish15121986@users.noreply.github.com>
2026-07-26 17:52:13 +0530

    Update output message in Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello I am Ashish Srivastava, I am fine automatically run");
+		System.out.println("Hello I am Ashish Srivastava, Job is automatically run ");
 	}
 }
 
